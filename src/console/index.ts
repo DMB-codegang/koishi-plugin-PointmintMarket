66e91a44d41dba9d62fe9a2d9a201ebf6de39cd4
yiyi <DMB-codegang@users.noreply.github.com>
2025-04-07 02:55:57 +0800

    1.1.0.dev-2 修复控制台无法正确保存标签的问题 优化了类型代码 新增控制台修改商品顺序的功能

--- src/console/index.ts
+++ src/console/index.ts
@@ -3,7 +3,13 @@ import { logs } from '../index'
 import { MarketItem } from '../types'
 import { market_database } from '../database'
 
-
+declare module '@koishijs/plugin-console' {
+    interface Events {
+      'get-Items'(): Promise<MarketItem[]>
+      'save-Items'(items: MarketItem[]): void
+      'delete-Item'(id: number): void
+    }
+  }
 
 export class consoleData {
     private ctx: Context
@@ -11,9 +17,17 @@ export class consoleData {
     constructor(ctx: Context) {
         this.ctx = ctx
         this.db = new market_database(ctx)
+        this.init()
     }
 
     init() {
-
+        this.ctx.console.addListener('get-Items', () => this.db.getAllMarketItem())
+        // db.getAllMarketItem()会返回一个Promise<MarketItem[]>
+        this.ctx.console.addListener('save-Items', async (data) => {
+            this.db.updateMarketItem(data)
+        })
+        this.ctx.console.addListener('delete-Item', (id: number) => {
+          this.ctx.market.deleteItemById(id)
+        })
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,13 @@ import { resolve } from 'path'
 // 导入pointmint插件的类型声明
 import { } from 'koishi-plugin-pointmint'
 
-import { market_database, MARKET_ITEMS_TABLE } from './database'
+import { market_database } from './database'
 import { MarketService } from './services'
-import { MarketItem } from './types'
-import { MarketItemRegisterOptions } from './types/services'
+
 import { registerCommands } from './command'
-import { } from './console'
+import { MarketItemRegisterOptions } from './types'
+import { consoleData } from './console'
+// 导入控制台类型定义
 import { Config } from './config'
 export * from './config'
 
@@ -26,43 +27,14 @@ export const inject = {
 
 export const logs = new Logger(name)
 
-declare module '@koishijs/plugin-console' {
-  interface Events {
-      'get-Items'(): Promise<MarketItem[]>
-      'save-Item'(items: MarketItem): void
-      'delete-Item'(id: number): void
-  }
-}
-
 export function apply(ctx: Context, config: Config) {
   const db = new market_database(ctx)
-  db.setupDatabase()
-
-  ctx.console.addListener('get-Items', () => db.getAllMarketItem())
-  // db.getAllMarketItem()会返回一个Promise<MarketItem[]>
-  ctx.console.addListener('save-Item', async (data) => {
-    // data为单个MarketItem，将该商品信息更新到Items中
-    const itemId = data.id
-    const Items = await db.getAllMarketItem()
-    const registeredItem = Items.find(item => item.id === itemId)
-    if (registeredItem) {
-      registeredItem.name = data.name
-      registeredItem.description = data.description
-      registeredItem.tags = data.tags
-      registeredItem.price = data.price
-      registeredItem.stock = data.stock
-      registeredItem.status = data.status
-      // 回写
-      db.updateMarketItem([registeredItem])
-    }
-  })
-  ctx.console.addListener('delete-Item', (id: number) => {
-    ctx.market.deleteItemById(id)
-  })
+  const cd = new consoleData(ctx)
   ctx.console.addEntry({
     dev: resolve(__dirname, '../client/index.ts'),
     prod: resolve(__dirname, '../dist'),
   })
+
 
   registerCommands(ctx, config)
   ctx.plugin(MarketService)
